cda394fcceebbc76fce46cdc4822105c27512132
DIR\jean-luc.ciapparelli <jean-luc.ciapparelli@accenture.com>
2022-09-29 10:32:22 +0200

    Created a Solution for First three days

---
+++ ConsoleApp1/Day2.cs
@@ -0,0 +1,108 @@
+﻿
+
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace AdventOfCode2021
+{
+    public class Day2
+    {
+        private const string path = "Day2.txt";
+        public List<string> directions = new List<string> { };
+
+
+
+        public void SetUp()
+        {
+            try
+            {
+                directions = File.ReadAllLines(path).ToList();
+
+            }
+            catch
+            {
+                throw new FileNotFoundException("Couldn't find file");
+            }
+        }
+
+
+        public int SolutionPart1()
+        {
+            int horizontal = 0;
+            int depth = 0;
+
+            foreach (string line in directions)
+            {
+                string direction = line.Split(" ")[0];
+                int distance = int.Parse(line.Split(" ")[1]);
+
+                switch (direction.ToLower())
+                {
+                    case "forward":
+                        horizontal += distance;
+                        break;
+
+                    case "down":
+                        depth += distance;
+                        break;
+
+                    case "up":
+                        depth -= distance;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+            return horizontal * depth;
+        }
+
+
+        public long SolutionPart2()
+        {
+            int horizontal = 0;
+            int depth = 0;
+            int aim = 0;
+
+            foreach (string line in directions)
+            {
+                string direction = line.Split(" ")[0];
+                int distance = int.Parse(line.Split(" ")[1]);
+
+                switch (direction.ToLower())
+                {
+                    case "forward":
+                        horizontal += distance;
+                        depth += aim * distance;
+                        break;
+
+                    case "down":
+                        aim += distance;
+                        break;
+
+                    case "up":
+                        aim -= distance;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+            return horizontal * depth;
+
+
+        }
+
+
+
+
+    }
+
+
+
+
+
+}
---
+++ ConsoleApp1/bin/Debug/net6.0/Day1.cs
@@ -0,0 +1,63 @@
+﻿namespace AdventOfCode2021
+{
+    public class Day1
+    {
+        private const string Path = "Day1.txt";
+        private List<int> depths = new List<int> { };
+        public void SetUp()
+        {
+            try
+            {
+                depths = File.ReadAllLines(Path)
+                    .Select(x => int.Parse(x.Trim()))
+                    .ToList();
+
+            }
+            catch
+            {
+                Console.WriteLine("Something went wrong in trying to locate the file. Please check the path.");
+            }
+        }
+
+
+        public int GiveSolutionPart1()
+        {
+            int HasIncreased = 0;
+
+            for (int i = 1; i < depths.Count; i++)
+            {
+                if (depths[i] > depths[i - 1])
+                {
+                    HasIncreased++;
+                }
+            }
+
+            return HasIncreased;
+        }
+
+        public int GiveSolutionPart2()
+        {
+            int HasIncreased3Window = 0;
+
+
+            for (int i = 1; i < depths.Count - 3; i++)
+            {
+                try
+                {
+                    if (depths[i + 3] > depths[i])
+                    {
+                        HasIncreased3Window++;
+                    }
+
+                }
+                catch
+                {
+                    Console.WriteLine($"Error:Index out of range for value of i being {i}");
+                }
+            }
+            return HasIncreased3Window;
+        }
+
+
+    }
+}
